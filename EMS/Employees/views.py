988c146939ea1e98a7b670a4e92fa6e79ce4ff7b
basit <abdulbasitcube5py@gmail.com>
2021-09-16 12:31:44 +0500

    working with it

--- EMS/Employees/views.py
+++ EMS/Employees/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .models import Employees
 from .serializer import   EmployeesSerializer
 from rest_framework.views import APIView 
+from rest_framework import status
 
 
 
@@ -13,4 +14,11 @@ class EmployeeList(APIView):
         employee = Employees.objects.all()
         serialize_class = EmployeesSerializer(employee, many= True)
         return Response(serialize_class.data)
+    
+    def post(self,request, format = None):
+        serialize_class = EmployeesSerializer(data=request.data)
+        if serialize_class.is_valid():
+            serialize_class.save()
+            return Response(serialize_class.data,status.HTTP_201_CREATED)
+        return Response("data not added",status.HTTP_400_BAD_REQUEST)
         
